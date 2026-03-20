e9eaff34e5de154cf66d53c14d4c6a9d9ee60249
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-20 13:56:57 +0900

    Update 3-4.c

---
+++ code/3-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int max_of (int count, ...) {
+  va_list = ap;
+  va_start(ap, count);
+  for(int i=0;i<count;i++) 
+    val = va_arg(ap, int);
+  va_end(ap);
+  return val;
+}
+int main() {
+  printf("%d\n", max_of(4, 5, 12, 3, 9));
+  printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+  printf("%d\n", max_of(3, 100, 200, 300));
+
+  return 0;
+}
