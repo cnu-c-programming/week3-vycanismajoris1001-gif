57c7da9a40a69908cff993e90756e653a4e380ed
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-20 14:04:34 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int max_of (int count, ...) {
-  va_list ap;
-  va_start(ap, count);
-  int max = va_arg(ap, int);
-  for(int i=0;i<count;i++) {
-    if (i>max) {
-      max = i;
-    }
-  }
-  va_end(ap);
-  return val;
-}
-int main() {
-  printf("%d\n", max_of(4, 5, 12, 3, 9));
-  printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
-  printf("%d\n", max_of(3, 100, 200, 300));
+int max_of(int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+.
+    int max = va_arg(ap, int); 
 
-  return 0;
+.
+    for (int i = 1; i < count; i++) {
+        int cur = va_arg(ap, int); 
+        if (cur > max) {          
+            max = cur;            
+        }
+    }
+
+    va_end(ap);
+
+    return max; 
+}
+
+int main() {
+    printf("%d\n", max_of(4, 5, 12, 3, 9));
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    printf("%d\n", max_of(3, 100, 200, 300));
+
+    return 0;
 }
