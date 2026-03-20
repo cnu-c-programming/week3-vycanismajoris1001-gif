92838038a94921e9b1a42a25cbac778995d91cc1
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-20 14:15:15 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_sum(char c, int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+
+    if (c == 'S') { 
+        for (int i = 0; i < count; i++) {
+            printf("%s ", va_arg(ap, char*));
+        }
+    } 
+    else if (c == 'C') { 
+        for (int i = 0; i < count; i++) {
+            printf("%c", va_arg(ap, int)); 
+        }
+    } 
+    else if (c == 'D') { 
+        int total = 0;
+        for (int i = 0; i < count; i++) {
+            total += va_arg(ap, int);
+        }
+        printf("%d", total);
+    }
+
+    printf("\n"); 
+    va_end(ap);
+}
+
+int main() {
+    my_sum('S', 2, "Hello", "World");
+    
+    my_sum('C', 3, 'C', 'N', 'U');
+    
+    my_sum('D', 4, 10, 20, 30, 40);
+
+    return 0;
+}
