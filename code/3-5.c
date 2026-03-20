a758172ed1a35a40f7cb5e24b13e5f6db2580295
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-20 14:17:10 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -5,17 +5,18 @@ void my_sum(char c, int count, ...) {
     va_list ap;
     va_start(ap, count);
 
-    if (c == 'S') { 
+    if (c == 'S') {
         for (int i = 0; i < count; i++) {
-            printf("%s ", va_arg(ap, char*));
+            printf("%s", va_arg(ap, char*));
+            if (i < count - 1) printf(" ");
         }
     } 
-    else if (c == 'C') { 
+    else if (c == 'C') {
         for (int i = 0; i < count; i++) {
-            printf("%c", va_arg(ap, int)); 
+            printf("%c", va_arg(ap, int));
         }
     } 
-    else if (c == 'D') { 
+    else if (c == 'D') {
         int total = 0;
         for (int i = 0; i < count; i++) {
             total += va_arg(ap, int);
@@ -23,15 +24,13 @@ void my_sum(char c, int count, ...) {
         printf("%d", total);
     }
 
-    printf("\n"); 
+    printf("\n");
     va_end(ap);
 }
 
 int main() {
     my_sum('S', 2, "Hello", "World");
-    
     my_sum('C', 3, 'C', 'N', 'U');
-    
     my_sum('D', 4, 10, 20, 30, 40);
 
     return 0;
